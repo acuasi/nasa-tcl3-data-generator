8509e966338c1bac6ebaccd5af513ee5d6a3dd3d
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2018-06-14 13:32:52 -0800

    docs(example): Update the example folder with flight_data.py.
    Update the example folder with flight_data parser to generate FLIGHT_DATA.json file.

--- CNS1_Example/example.py
+++ CNS1_Example/example.py
@@ -1,7 +1,8 @@
-import cns1
+import cns1, flight_data
+
 mi_file_name = "2018-04-27 11-16-16_mission_insight.csv"
 dataflash_file_name = "2018-04-27 11-16-16.log"
 field_vars_file_name = "2018-04-27 11-16-16_field_variables.csv"
-outfile_name = "CNS1.json"
 
-cns1.generate(mi_file_name, dataflash_file_name, field_vars_file_name, outfile_name)
+cns1.generate(mi_file_name, dataflash_file_name, field_vars_file_name, "CNS1.json")
+flight_data.generate(mi_file_name, dataflash_file_name, "FLIGHT_DATA.json")
